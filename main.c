61e14f8d8611659508f5ec491a09c154745f682b
Deivid R. S. <deivid@schiitz.com>
2024-06-08 16:17:51 -0400

    RR

--- main.c
+++ main.c
@@ -12,15 +12,15 @@
 #endif
 
 
-struct FCFS
-{
-    char p[100];
-    int t;
-    int t_wait;
-};
-
 void FCFS(char *file_in)
 {
+    struct FCFS
+    {
+        char p[100];
+        int t;
+        int t_wait;
+    };
+
     printf("FCFS\n");
 
     /*** READ FILE ***/
@@ -68,7 +68,71 @@ void Priority(char *file_in)
 
 void RoundRobin(char *file_in)
 {
-    pdebug("RoundRobin\n");
+    printf("RoundRobin\n");
+
+    struct RR_process
+    {
+        char p[100];
+        int burst_time;
+    };
+
+    struct RR
+    {
+        int quantum;
+        int n;
+        struct RR_process *process;
+    };
+
+    /*** READ FILE ***/
+    FILE *input_arq;
+
+    input_arq = fopen(file_in, "r");
+    if (input_arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo de entrada %s.\n", file_in);
+        exit(1);
+    }
+
+    struct RR rr;
+    fscanf(input_arq, "%d %d", &rr.n, &rr.quantum);
+
+    pdebug("n: %d\n", rr.n);
+    pdebug("quantum: %d\n", rr.quantum);
+
+    rr.process               = (struct RR_process *) malloc(rr.n * sizeof(struct RR_process));
+    int max_burst_time       = 0;
+    int max_burst_time_index = 0;
+    for (int i = 0; i < rr.n; i++)
+    {
+        fscanf(input_arq, "%s %d", rr.process[i].p, &rr.process[i].burst_time);
+
+        if (rr.process[i].burst_time > max_burst_time)
+        {
+            max_burst_time       = rr.process[i].burst_time;
+            max_burst_time_index = i;
+        }
+    }
+
+    fclose(input_arq);
+
+    // Print the process
+    int time = 0;
+    printf("Processo | Tempo_executando\n");
+    while (rr.process[max_burst_time_index].burst_time > 0)
+    {
+        for (int i = 0; i < rr.n; i++)
+        {
+            if (rr.process[i].burst_time > 0)
+            {
+                if (rr.process[i].burst_time < rr.quantum)
+                    time += rr.process[i].burst_time;
+                else
+                    time += rr.quantum;
+                rr.process[i].burst_time -= rr.quantum;
+                printf("   %s    |        %d        \n", rr.process[i].p, time);
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
